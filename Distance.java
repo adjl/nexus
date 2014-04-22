92e86ff7c092aeaa9009e8d744613654be72165d
Adei Josol <helena.josol@gmail.com>
2014-04-22 12:34:23 +0100

    Close #12: set beam speed equal to head size

--- Distance.java
+++ Distance.java
@@ -1,3 +1,19 @@
 enum Distance {
-  FAR, MIDDLE, NEAR
-};
+
+  // Modify these as preferred:
+  // --------------------------
+  FAR(5),
+  MIDDLE(7),
+  NEAR(9);
+  // --------------------------
+
+  final int distance;
+
+  Distance(int distance) {
+    this.distance = distance;
+  }
+
+  int getDistance() {
+    return distance;
+  }
+}
