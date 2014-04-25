2a72a0fbc3f10bf3a54882f91942eae5ba99fcaf
Adei Josol <helena.josol@gmail.com>
2014-04-26 00:28:23 +0100

    Remove comment lines

--- Distance.java
+++ Distance.java
@@ -1,11 +1,8 @@
 enum Distance {
 
-  // Modify these as preferred:
-  // --------------------------
   FAR(2, 2),
   MIDDLE(4, 4),
   NEAR(8, 8);
-  // --------------------------
 
   final int size;
   final int speed;
